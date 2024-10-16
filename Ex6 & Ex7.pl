9cca85d83d1ea31dd803566c191df47471489f9b
Diego Rockenbach <89611181+DiegoRockenbach@users.noreply.github.com>
2024-10-16 17:41:29 -0300

    respostas dos exercícios

---
+++ Ex6 & Ex7.pl
@@ -0,0 +1,10 @@
+%6
+yearSubtraction(AnoAtual,AnoBigFish,Res) :- Res is AnoAtual-AnoBigFish.
+
+%7
+genero(adamSandler, comedia).
+genero(liamNeeson, acao).
+genero(liamNeeson, suspense).
+genero(jimCarrey, comedia).
+genero(jasonMomoa, acao).
+genero(jasonMomoa, drama).
